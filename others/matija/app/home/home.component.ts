fc82061cd7ad8e382c585f9a894f77a6437e49e5
NickIliev <nikolay.iliev@telerik.com>
2019-01-31 14:21:23 +0200

    add

---
+++ others/matija/app/home/home.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from "@angular/core";
+import * as calendarModule from "nativescript-ui-calendar";
+import { Color } from "color";
+
+@Component({
+    selector: "Home",
+    moduleId: module.id,
+    templateUrl: "./home.component.html",
+    styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+    categoricalSource: { Country: string, Amount: number }[] = [
+        { Country: "7", Amount: 3 },
+        { Country: "8", Amount: 2 },
+        { Country: "9", Amount: 6 },
+        { Country: "10", Amount: 8, },
+        { Country: "11", Amount: 5, },
+        { Country: "12", Amount: 0, },
+         { Country: "13", Amount: 9, }
+    ];
+
+    calendarEvents = [];
+
+    constructor() {
+        // Creating dummy events
+        let events = [];
+        let now = new Date();
+        let startDate;
+        let endDate;
+        let colors = [new Color(200, 188, 26, 214), new Color(220, 255, 109, 130), new Color(255, 55, 45, 255), new Color(199, 17, 227, 10), new Color(255, 255, 54, 3)];
+        for (let i = 1; i < 10; i++) {
+            startDate = new Date(now.getFullYear(), now.getMonth(), i * 2, 1);
+            endDate = new Date(now.getFullYear(), now.getMonth(), (i * 2), 3);
+            let event = new calendarModule.CalendarEvent("event " + i, startDate, endDate, false, colors[i * 10 % (colors.length - 1)]);
+            events.push(event);
+            if (i % 3 == 0) {
+                event = new calendarModule.CalendarEvent("second " + i, startDate, endDate, true, colors[i * 5 % (colors.length - 1)]);
+                events.push(event);
+            }
+        }
+        this.calendarEvents = events;
+    }
+
+    ngOnInit(): void {
+    }
+
+
+
+ 
+}
