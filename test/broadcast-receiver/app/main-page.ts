f60b02eed57ace97a67dc6414dda394b9399e6c2
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2019-02-19 16:03:07 +0200

    add

--- test/broadcast-receiver/app/main-page.ts
+++ test/broadcast-receiver/app/main-page.ts
@@ -1,5 +1,5 @@
 
-import { EventData, fromObject } from "tns-core-modules/data/observable";
+import { EventData, fromObject, Observable } from "tns-core-modules/data/observable";
 import { Page } from "tns-core-modules/ui/page";
 import { isAndroid } from "tns-core-modules/platform";
 import * as applicationModule from "tns-core-modules/application";
@@ -9,17 +9,17 @@ let vm;
 
 export function onNavigatingTo(args: EventData) {
     const page = <Page>args.object;
-    vm = fromObject({});
+    vm = fromObject({
+        "info": "Using Android Broadcast Receiver \nto check the battery life",
+        "batteryLife": 42
+    })
 
-    vm.set("info", "Using Android Broadcast Receiver \nto check the battery life");
-    vm.set("batteryLife", 0);
-    
     page.bindingContext = vm;
 }
 
 export function startReceiver() {
     if (isAndroid) {
-        receiver = applicationModule.android.registerBroadcastReceiver(
+        let receiver = applicationModule.android.registerBroadcastReceiver(
             android.content.Intent.ACTION_BATTERY_CHANGED,
             (context, intent) => {
                 const level = intent.getIntExtra(android.os.BatteryManager.EXTRA_LEVEL, -1);
@@ -37,6 +37,12 @@ export function startReceiver() {
 export function stopReceiver() {
     if (isAndroid) {
         // as receiver is undefeind we can't find the broadcast receiver which we want to unregister from
-        applicationModule.android.unregisterBroadcastReceiver(receiver);
+        applicationModule.android.unregisterBroadcastReceiver(android.content.Intent.ACTION_BATTERY_CHANGED);
     }
+}
+
+export function navigate(args) {
+    let frame = args.object.page.frame;
+    console.log(frame);
+    frame.navigate("sub-page")
 }
