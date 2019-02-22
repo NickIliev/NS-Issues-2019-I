296f54116b66c015ca07fd65252eee806baeb86e
NickIliev <nikolay.iliev@telerik.com>
2019-02-22 13:22:02 +0200

    add

---
+++ others/t-1389298/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+import * as twilio from "nativescript-twilio-video";
+
+@Component({
+    selector: "ns-app",
+    moduleId: module.id,
+    templateUrl: "./app.component.html"
+})
+export class AppComponent { }
