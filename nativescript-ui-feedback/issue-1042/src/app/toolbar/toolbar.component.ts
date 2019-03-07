031fbd30db5cbdb86119ba11775cc36cfa1f3729
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2019-03-07 11:02:35 +0200

    add

---
+++ nativescript-ui-feedback/issue-1042/src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'toolbar',
+    moduleId: module.id,
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.css']
+})
+
+export class ToolbarComponent {
+    // using 'master' property from the parent component (MainComponent)
+    @Input('item') item: any;
+
+    // outputs the events fired from from our custom toolbar 
+    @Output() toolbar: EventEmitter<string> = new EventEmitter<string>();
+
+    goToFirst() {
+        this.toolbar.emit("goToFirst");
+    }
+
+    goToSecond() {
+        this.toolbar.emit("goToSecond");
+    }
+
+    goToThird() {
+        this.toolbar.emit("goToThird");
+    }
+}
