7ed16a223dab6a5cab1e77e99d99d062e1d149cc
NickIliev <nikolay.iliev@telerik.com>
2019-05-27 13:34:03 +0300

    add

---
+++ NativeScript/issue-7284/app/main-view-model.ts
@@ -0,0 +1,37 @@
+import { Observable } from "tns-core-modules/data/observable";
+
+export class HelloWorldModel extends Observable {
+
+    private _message: string;
+    res: any;
+
+    constructor() {
+        super();
+
+        this.res = android.R;
+    }
+
+    get message(): string {
+        return this._message;
+    }
+
+    set message(value: string) {
+        if (this._message !== value) {
+            this._message = value;
+            this.notifyPropertyChange("message", value);
+        }
+    }
+
+    onTap() {
+        this.updateMessage();
+    }
+
+    private updateMessage() {
+        console.log(android.R.color.holo_blue_bright.toString());
+        this.message = android.R.color.holo_blue_bright.toString();
+
+        // or
+        // this.message = this.res.color.holo_blue_bright;
+    }
+        
+}
