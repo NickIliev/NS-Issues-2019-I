06d3e98a14f1f8b780529d2a0fd1289122e5e995
NickIliev <nikolay.iliev@telerik.com>
2019-07-23 15:14:13 +0300

    add

---
+++ NativeScript/issue-7559/src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import * as app from "tns-core-modules/application";
+import { RouterExtensions } from "nativescript-angular/router";
+
+@Component({
+    selector: "Home",
+    moduleId: module.id,
+    templateUrl: "./home.component.html"
+})
+export class HomeComponent implements OnInit {
+
+    constructor(private _router: RouterExtensions) {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Init your component properties here.
+    }
+
+    navigateToHome() {
+        this._router.navigate(["search"]);
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.showDrawer();
+    }
+}
