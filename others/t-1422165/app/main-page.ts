b8e53c8a32bad35632a84d8bf95450468d3607da
NickIliev <nikolay.iliev@telerik.com>
2019-07-29 12:51:39 +0300

    add

---
+++ others/t-1422165/app/main-page.ts
@@ -0,0 +1,21 @@
+import { EventData } from "tns-core-modules/data/observable";
+import { Page } from "tns-core-modules/ui/page";
+import { HelloWorldModel } from "./main-view-model";
+import { requestPermissions } from "nativescript-permissions";
+const phone = require( "nativescript-phone" );
+
+export function navigatingTo(args: EventData) {
+
+    const page = <Page>args.object;
+    page.bindingContext = new HelloWorldModel();
+
+    requestPermissions(["android.permission.CALL_PHONE"]).then(() => {
+        console.log(`Permissionsgranted!`);
+    }).catch(err => {
+        console.log(`Permissions fdenied - time for plan B!`);
+    })
+}
+
+export function makeCall() {
+    phone.dial("212-555-1234",false);
+}
